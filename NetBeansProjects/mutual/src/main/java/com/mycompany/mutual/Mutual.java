81da4ab95e3dee8f8ee9945e8fb2df05d4496935
DiegoCardenasCriado <dcardenascriado@gmail.com>
2022-09-24 19:24:39 -0500

    second commit

---
+++ NetBeansProjects/mutual/src/main/java/com/mycompany/mutual/Mutual.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mutual;
+
+/**
+ *
+ * @author Diego_Cárdenas
+ */
+public class Mutual {
+
+    public static void main(String[] args) {
+       
+        /*
+        5
+        int[] myArray = {1,2,1,3,3,1,2,1,5,1};
+        
+        for (int i = 1; i <= 5; i++) {
+            String cantidad = "";
+            for (int j = 0; j < myArray.length; j++) {
+                if ( i == myArray[j] ) {
+                    cantidad += "*";
+                }
+            }
+            System.out.println(i+": "+cantidad);
+            
+        }
+        */
+        
+        
+        /*
+        6
+        int[] myArray = {1,2,2,2,7,7,7,8,8,8};
+        int cont = 0;
+        int num = 0;
+        
+        for (int i = 1; i <= 9; i++) {
+            int aux = 0;
+            for (int j = 0; j < myArray.length; j++) {
+                if ( i == myArray[j] ) {
+                    aux++;
+                }
+            }
+            if ( cont < aux ) {
+                num = i;
+                cont = aux;
+            }
+
+        }
+        System.out.println("Longest: "+cont);
+        System.out.println("Number: "+num);
+        */
+        
+        /*
+        7
+        int[] myArray = {1,3,4,2,7,0};
+        
+        for (int i = 0; i < myArray.length; i++) {
+            for (int j = 0; j < myArray.length; j++) {
+                if ( myArray[i] < myArray[j] ) {
+                    if ( myArray[i] + myArray[j] == 10) {
+                        System.out.println(myArray[i] +" "+ myArray[j]);
+                    }
+                }
+            }
+        }
+        */
+        
+    }
+}
